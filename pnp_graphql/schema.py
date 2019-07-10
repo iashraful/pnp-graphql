70e365dc2e9a4fb705b91dc83bc2f708c66f8af2
Ashraful Islam <ashraful@field.buzz>
2019-07-10 17:17:24 +0600

    Updated mutation making. But still need to work on it

--- pnp_graphql/schema.py
+++ pnp_graphql/schema.py
@@ -1,9 +1,8 @@
 import graphene
-from graphene import Mutation
 
-from pnp_graphql.utils.mutation_helpers import prepare_mutation_class_attributes
-from pnp_graphql.utils.query_helpers import get_query_attributes
 from pnp_graphql.utils.class_factory import class_factory
+from pnp_graphql.utils.mutation_helpers import prepare_mutation_classes
+from pnp_graphql.utils.query_helpers import get_query_attributes
 
 # Getting query attributes with mapping
 _query_attrs = get_query_attributes()
@@ -15,7 +14,15 @@ _query = class_factory(__class_name='Query', base_classes=(graphene.ObjectType,)
 #     success = graphene.Boolean()
 
 
-_mutation = class_factory(__class_name='Mutation', base_classes=(Mutation,), )
+_mutation_base_classes = prepare_mutation_classes()
+_mutation_attrs = {}
+for _c in _mutation_base_classes:
+    _mutation_attrs[_c.__name__.lower()] = getattr(_c, 'Field')()
+_mutation = class_factory(
+    __class_name='Mutation', 
+    base_classes=(graphene.ObjectType,),
+    **_mutation_attrs
+)
 
 # This schema will be declared to settings
 schema = graphene.Schema(query=_query, mutation=_mutation)
--- pnp_graphql/utils/mutation_helpers.py
+++ pnp_graphql/utils/mutation_helpers.py
@@ -1,14 +1,15 @@
 import graphene
+from graphene import Mutation
 
 from pnp_graphql.constants import MODEL_INPUT_ATTR, MODEL_TYPE_ATTR
 from pnp_graphql.utils.class_factory import class_factory
 from pnp_graphql.utils.managers import get_model_fields, get_enabled_app_models
+from pnp_graphql.input_generator import GraphQlInputGenerator
 
 
-def prepare_mutate(_class, model, **kwargs):
+def prepare_mutate(model, **kwargs):
     @staticmethod
     def mutate(root, info, input=None):
-        success = True
         _fields = get_model_fields(model=model, flat=True)
         _data = {}
         for f in _fields:
@@ -16,23 +17,27 @@ def prepare_mutate(_class, model, **kwargs):
                 _data[f] = getattr(input, f)
         instance = model(**_data)
         instance.save()
-        return _class(success=success, data=instance)
+        return Mutation(data=instance)
+    return mutate
 
 
 def prepare_mutation_class_attributes(model):
     _input_type = getattr(model, MODEL_INPUT_ATTR, None)
     _query_type = getattr(model, MODEL_TYPE_ATTR, None)
     _mutation_class_attrs = {
-        'Meta': class_factory(__class_name='Arguments', base_classes=(), input=_input_type),
-        'success': graphene.Boolean(),
-        'data': graphene.Field(_query_type)
+        'Arguments': class_factory(__class_name='Arguments', base_classes=(), input=_input_type()),
+        'data': graphene.Field(_query_type),
+        'mutate': prepare_mutate(model)
     }
     return _mutation_class_attrs
 
 
 def prepare_mutation_classes():
     _models = get_enabled_app_models()
+    GraphQlInputGenerator.generate_input_types()
     _classes = []
     for m in _models:
         _attrs = prepare_mutation_class_attributes(model=m)
-        _class = class_factory(__class_name=m.__name__ + '')
+        _class = class_factory(__class_name=m.__name__ + 'Create', base_classes=(Mutation,), **_attrs)
+        _classes.append(_class)
+    return tuple(_classes)
