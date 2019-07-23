5b070966f40760cc8685ceb68725e1c189345617
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-07-23 22:17:52 +0600

    Updated setup file for support from django 2.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        'django>=2.2.3',
-        'graphene-django>=2.3',
+        'django>=2.0',
+        'graphene-django>=2.1',
     ]
 )
